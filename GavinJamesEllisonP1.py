a09c603c77e08d272e233eb5dc72474f6c9c8c99
gellison0686 <91559934+gellison0686@users.noreply.github.com>
2022-03-03 14:38:16 -0500

    Add files via upload

--- GavinJamesEllisonP1.py
+++ GavinJamesEllisonP1.py
@@ -74,3 +74,99 @@ while notDone:
         print("Too Bad (Cool Emoji With Sunglasses)")
     else:
         print("The Sourcerer's Stone? ")
+
+        answerb = input("Kick-Ass? ")
+    if answerb == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Kick-Ass 2? ")
+
+        answerc = input("Left Behind? ")
+    if answerc == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("The Croods? ")
+
+        answerd = input("Moonstruck? ")
+    if answerd == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("National Treasur: Book of Secrets? ")
+
+        answere = input("The Frozen Ground? ")
+    if answere == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("The Wicker Man? ")
+
+        answerf = input("Primal? ")
+    if answerf == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("City Of Angles? ")
+
+        answerg = input("Season of the Witch? ")
+    if answerg == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Wild at Heart? ")
+
+        answerh = input("Raising Arizona? ")
+    if answerh == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Fast Times at Ridgemont High? ")
+
+        answeri = input("8MM? ")
+    if answeri == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("The Croods a New Age? ")
+
+        answerj = input("Mom and Dad? ")
+    if answerj == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Color Out of Space? ")
+
+        answerk = input("Snake Eyes? ")
+    if answerk == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Seeking Justice? ")
+
+        answerl = input("Stolen? ")
+    if answerl == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Valley Girl? ")
+
+        answerm = input("Vampires Kiss? ")
+    if answerm == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("A Score to Settle? ")
+
+        answern = input("Bad Lieutenant: Port of Call? ")
+    if answern == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Jiu Jitsu? ")
+
+        answero = input("Adaption? ")
+    if answero == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("World Trade Centre? ")
+
+        answerp = input("211? ")
+    if answerp == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Matchstick Men? ")
+
+        answerq = input("Grand Isle? ")
+    if answerq == "Y":
+        print("Too Bad (Cool Emoji With Sunglasses)")
+    else:
+        print("Kill Chain? ")
